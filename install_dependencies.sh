931fe7aed2ebd625f4b13f0ccbd6e47062d49699
Aayush Sharma <aayushsharma889@gmail.com>
2019-05-29 12:13:44 +0200

    added stow script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -22,6 +22,7 @@ dependencies=(
     'syncthing'                                         # for file sync
     'stow'                                              # gnu stow
 )
+
 polybarDependencies=(
     'cmake' 
     'gcc-c++'
@@ -53,3 +54,10 @@ fi
 
 echo
 echo "Installation complete"
+echo
+read -p "Do you want to setup all the config files? [Y/n] " response
+if [ ! $response -o $response = 'y' -o $response = 'Y' ]
+then
+    chmod +x ./install_config.sh
+    ./install_config.sh
+fi
