a50a9edb2af79059e65074c19407fc1c63bdc2fe
Aayush Sharma <aayushsharma889@gmail.com>
2020-09-24 21:00:33 +0200

    natural scrolling only applies to touchpad

--- configure_xinput.sh
+++ configure_xinput.sh
@@ -1,10 +1,12 @@
 # ENABLES -> NaturalScrolling and Click-on-Tap emulation
 
+# TOUCHPAD
 FOLDER="/etc/X11/xorg.conf.d"
 FILE="30-touchpad.conf"
 
 config="Section \"InputClass\" \n
             \t Identifier \"touchpad\" \n
+            \t MatchisTouchPad \"on\" \n
             \t Driver \"libinput\" \n
             \t Option \"NaturalScrolling\" \"true\" \n
             \t Option \"Tapping\" \"on\" \n
