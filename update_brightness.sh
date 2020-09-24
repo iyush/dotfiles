f0aac058ee131a84b0ff49b9c081def74859a07a
Aayush Sharma <aayushsharma889@gmail.com>
2020-09-24 21:02:13 +0200

    brightness does not go above 100

--- update_brightness.sh
+++ update_brightness.sh
@@ -1,4 +1,5 @@
 display=$(xrandr | grep " connected" | cut -f1 -d " ")
+
 delta=$1
 current=$(xrandr --verbose | grep -i brightness | cut -f2 -d " " | head -n1)
 added=$(echo "$delta + $current" | bc -l)
@@ -6,12 +7,9 @@ added=$(echo "$delta + $current" | bc -l)
 greater_than_1=$(echo "$added > 1" | bc -l)
 smaller_than_0=$(echo "$added < 0" | bc -l)
 
-if [ "$greater_than_1" -eq "1" ] 
+if [ $greater_than_1 -eq '1' ] 
 then
-    added="1"
-elif [ "$smaller_than_0" -eq "1" ] 
-then
-    added="0"
+    exit
 fi
 
 xrandr --output $display --brightness $added
