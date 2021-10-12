f798126bc55828a3d164427dd6521d8181b5e0dc
Aayush Sharma Acharya <a.acharya@cellumation.com>
2021-10-12 17:06:46 +0200

    updated polybar with new icons and modules, refactored as well

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-polybar white &
-#polybar bar2 &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload white &
+  done
+else
+    polybar --reload white &
+fi
 
 echo "Bars launched..."
