38809acad958b1ee51a8590748accadbaa9b3055
Aayush Sharma Acharya <a.acharya@cellumation.com>
2021-11-12 17:13:25 +0100

    added lsp support for cpp, ts and vue js files

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,11 @@
-;; PACKAGE -- summary
+ ;; PACKAGE -- summary
 ;;; Commentary:
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; MELPA PACKAGES
+;; MELPA PACKAGE
+(setq custom-file "~/.emacs.d/custom.el")
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
@@ -18,5 +19,44 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file "~/.emacs.d/configuration.org")
-(put 'downcase-region 'disabled nil)
+;; (org-babel-load-file "~/.emacs.d/configuration.org")
+;; (put 'downcase-region 'disabled nil)
+
+;; Emacs LSP
+
+(use-package which-key
+  :ensure)
+(which-key-mode)
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :init (setq lsp-keymap-prefix "C-c l")
+  :config (lsp-enable-which-key-integration t))
+
+
+
+;; Vue js and Typescript mode
+
+(use-package typescript-mode
+  :ensure t
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+(use-package vue-mode
+  :ensure t
+  :mode "\\.vue\\'"
+  :hook (vue-mode . lsp))
+
+(add-hook vue-mode-hook lambda()(lsp))
+
+
+;; Cpp and C with ccls language server
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+(load custom-file)
